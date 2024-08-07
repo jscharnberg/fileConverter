3cff7de4980186cd06dad0ea6780fac20b6956fb
jscharnberg <privat@jscharnberg.com>
2024-08-07 11:58:47 +0200

    responsive and changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
 import FileUploader from './Components/FileUploader'
-
+import { ThemeProvider } from "@/Components/theme-provider"
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Free File Converter</h1>
-        <p>Introducing FileFlex – your go-to online tool for unlimited and free multimedia conversion, all processed locally on your device for enhanced privacy and security. Easily convert images, audio, and videos without any restrictions. Start converting now and streamline your content effortlessly with FileFlex!</p>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <h1 className='heading'>Jan's file converter</h1>
+        <p>Introducing Jan's file converter – your go-to online tool for unlimited and free multimedia conversion, all processed locally on your device for enhanced privacy and security. Easily convert images, audio, and videos without any restrictions. Start converting now and streamline your content effortlessly with FileFlex!</p>
         <FileUploader />
-      </div>
+      </ThemeProvider>
     </>
   )
 }
--- src/Components/FileUploader.tsx
+++ src/Components/FileUploader.tsx
@@ -1,11 +1,27 @@
 import React, { useState, useRef } from 'react';
 import './FileUploader.css';
+import { faX, faUpload } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button } from "@/Components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/Components/ui/card"
+
+library.add(faX);
+library.add(faUpload);
 
 export default function FileUploader() {
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
     const [outputFormat, setOutputFormat] = useState<string>('');
     const [fileType, setFileType] = useState<string>('');
+    const [isConverting, setIsConverting] = useState<boolean>(false);
     const fileInputRef = useRef<HTMLInputElement>(null);
 
     const handleButtonClick = () => {
@@ -31,11 +47,15 @@ export default function FileUploader() {
             return;
         }
 
+        setIsConverting(true);
+
         if (fileType === 'image') {
             convertImage();
         } else if (fileType === 'video') {
             convertVideo();
         }
+
+        setIsConverting(false);
 
     };
 
@@ -108,43 +128,103 @@ export default function FileUploader() {
         xhr.send();
     };
 
+    const handleDragOver = (event: React.DragEvent) => {
+        event.preventDefault();
+    };
+
+    const handleDrop = (event: React.DragEvent) => {
+        event.preventDefault();
+        if (event.dataTransfer.files) {
+            const file = event.dataTransfer.files[0];
+            setSelectedFile(file);
+            setFileType(file.type.split('/')[0]); // 'image' or 'video'
+        }
+    };
+
+    const closeCard = () => {
+        setSelectedFile(null);
+        setOutputFormat('');
+        setFileType('');
+        setIsConverting(false);
+        if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+        }
+    };
+
     return (
         <div className="file-uploader">
             {!selectedFile && (
-                <button onClick={handleButtonClick}>Select File</button>
+                <>
+                    <Card className='card' onClick={handleButtonClick} onDragOver={handleDragOver} onDrop={handleDrop}>
+                        <CardHeader>
+                        </CardHeader>
+                        <CardContent>
+                            <FontAwesomeIcon icon="upload" size='5x' className='upload-icon' />
+                            <p className='upload-text'>Click, or drop a file here</p>
+
+                            <input
+                                type="file"
+                                accept="image/*,video/*"
+                                ref={fileInputRef}
+                                style={{ display: 'none' }}
+                                onChange={handleFileChange}
+                            />
+                        </CardContent>
+                        <CardFooter></CardFooter>
+                    </Card>
+                </>
             )}
-            <input
-                type="file"
-                accept="image/*,video/*"
-                ref={fileInputRef}
-                style={{ display: 'none' }}
-                onChange={handleFileChange}
-            />
             {selectedFile && (
                 <>
-                    {fileType === 'image' && (
-                        <select value={outputFormat} onChange={handleFormatChange}>
-                            <option value="png">PNG</option>
-                            <option value="jpeg">JPEG</option>
-                            <option value="webp">WEBP</option>
-                            <option value="bmp">BMP</option>
-                            <option value="gif">GIF</option>
-                        </select>
-                    )}
-                    {fileType === 'video' && (
-                        <select value={outputFormat} onChange={handleFormatChange}>
-                            <option value="mp4">MP4</option>
-                            <option value="webm">WEBM</option>
-                            <option value="ogv">OGV</option>
-                        </select>
-                    )}
-                    <button onClick={handleConvertAndDownload}>Convert and Download</button>
                     {/* <button onClick={handleConvertAndDownload}>Convert and Download</button> */}
                     {/* {downloadUrl && (
                         <a href={downloadUrl} download={`converted-file.${outputFormat}`}>
                             Download {outputFormat.toUpperCase()}
                         </a>
                     )} */}
+                    <Card className='card'>
+                        <CardHeader>
+                            <CardTitle>Convert File</CardTitle>
+                        </CardHeader>
+                        <CardContent id='container'>
+                            <div className='file-name'>
+                                {selectedFile.name}
+                            </div>
+                            <div className='convert'>
+                                Convert to: &nbsp;
+                                {fileType === 'image' && (
+                                    <select value={outputFormat} onChange={handleFormatChange}>
+                                        <option value="png">PNG</option>
+                                        <option value="jpeg">JPEG</option>
+                                        <option value="webp">WEBP</option>
+                                        <option value="bmp">BMP</option>
+                                        <option value="gif">GIF</option>
+                                    </select>
+                                )}
+                                {fileType === 'video' && (
+                                    <select value={outputFormat} onChange={handleFormatChange}>
+                                        <option value="mp4">MP4</option>
+                                        <option value="webm">WEBM</option>
+                                        <option value="ogv">OGV</option>
+                                    </select>
+                                )}
+                            </div>
+                            <div className='close'>
+                                <Button variant="ghost" onClick={closeCard}>
+                                    <FontAwesomeIcon icon="x" />
+                                </Button>
+                            </div>
+                        </CardContent>
+                        <CardFooter className='flex card-footer'>
+                            <Button className='flex-left footer-button close-smallScreen' onClick={closeCard}>
+                                Close
+                            </Button>
+                            <Button className='flex-left footer-button' onClick={handleConvertAndDownload}>
+                                {isConverting ? <span className="spinner"></span> : 'Convert and Download'}
+
+                            </Button>
+                        </CardFooter>
+                    </Card>
                 </>
             )}
         </div>
