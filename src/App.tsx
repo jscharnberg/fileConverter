64322fd22bb6926a14825b1885d10849c833427f
jscharnberg <privat@jscharnberg.com>
2024-08-07 14:10:50 +0200

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import FileUploader from './Components/FileUploader'
 import { ThemeProvider } from "@/Components/theme-provider"
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
@@ -8,8 +9,9 @@ function App() {
     <>
       <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
         <h1 className='heading'>Jan's file converter</h1>
-        <p>Introducing Jan's file converter – your go-to online tool for unlimited and free multimedia conversion, all processed locally on your device for enhanced privacy and security. Easily convert images, audio, and videos without any restrictions. Start converting now and streamline your content effortlessly with FileFlex!</p>
+        <p>Introducing Jan's file converter – your go-to open source online tool for unlimited and free multimedia conversion, all processed locally on your device for enhanced privacy and security. Easily convert images, audio, and videos without any restrictions. Start converting now and streamline your content effortlessly with FileFlex!</p>
         <FileUploader />
+        <Footer />
       </ThemeProvider>
     </>
   )
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faGithub, faInstagram, faLinkedin);
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-icons">
+                <a href="https://github.com/jscharnberg/fileConverter" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+            </div>
+            <div className="footer-links">
+                <a href="/">Datenschutz</a>
+                <a href="/">Impressum</a>
+            </div>
+        </footer>
+    );
+}
